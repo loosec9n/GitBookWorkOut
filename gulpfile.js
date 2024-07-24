d9b741c890c5adafa1fe2d6fa20bfd654473b39e
Justin John <justin.john@netsentries.com>
2024-07-24 13:09:09 +0530

    2nd update into main

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 const gulp = require("gulp");
 const gulpLoadPlugins = require("gulp-load-plugins");
+const gutil = require("gulp-util");
 
 const $ = gulpLoadPlugins();
 
 gulp.task("publish", () => {
   console.log("publish gitbook to github pages");
-  return gulp.src("**/*").pipe(
-    $.ghPages({
-      origin: "origin",
-      branch: "main",
-    })
-  );
+  return gulp
+    .src("**/*")
+    .pipe(
+      $.ghPages({
+        origin: "origin",
+        branch: "main",
+      })
+    )
+    .on("error", (err) => {
+      // Error handling
+      gutil.log(gutil.colors.red("[Error]"), err.toString());
+      this.emit("end");
+    });
 });
